c0cb8c44f2b85df6f25f011b0f6ffef82f257d32
Bruno Amaral <amaralbf@gmail.com>
2022-11-01 23:28:59 -0300

    feat: Add TextAsset

--- src/mkdocs_ml/asset.py
+++ src/mkdocs_ml/asset.py
@@ -14,3 +14,17 @@ class MatplotlibAsset:
     @staticmethod
     def generate_filename(key):
         return f'{key}.png'
+
+
+class TextAsset:
+    def __init__(self, key, data: str):
+        self.filename = self.generate_filename(key)
+        self.data = data
+
+    def save(self, data_dir: Path):
+        with open(data_dir / self.filename, 'wt') as f:
+            f.write(self.data)
+
+    @staticmethod
+    def generate_filename(key):
+        return f'{key}.txt'
--- tests/unit/test_asset.py
+++ tests/unit/test_asset.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 
-from mkdocs_ml.asset import MatplotlibAsset
+from mkdocs_ml.asset import MatplotlibAsset, TextAsset
 
 # import pytest
 
@@ -20,3 +20,21 @@ def test_plot_png_creation(tmp_path: Path):
     assert len(files) == 1
     assert KEY in str(files[0])
     assert str(files[0]).endswith('.png')
+
+
+def test_text_file_creation_from_string(tmp_path: Path):
+    TEXT = 'Text to add to the docs.'
+    KEY = 'text'
+
+    asset = TextAsset(key=KEY, data=TEXT)
+    asset.save(data_dir=tmp_path)
+
+    files = list(tmp_path.iterdir())
+
+    assert len(files) == 1
+
+    filename = str(files[0])
+    assert KEY in filename and filename.endswith('.txt')
+
+    with open(tmp_path / filename, 'rt') as f:
+        assert f.read() == TEXT
